f92f3155b0d3cc3101dc51ab7bab481c0eb0bb6e
cubist38 <74866985+cubist38@users.noreply.github.com>
2023-05-19 23:29:43 +0700

    a

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # classification_with_knowledge_distillation
 
-## Discription
+## Description
 Author: Vuong Gia Huy
 
-The usage of knowledge distillation technique in classification problems. 
+The usage of knowledge distillation technique in classification problems. In this repo, i used the dataset of `EDWARD ZHANG` ([link](https://www.kaggle.com/datasets/sshikamaru/fruit-recognition)) on kaggle. I had balanced this dataset before i applied for training with knowledge distillation technique.
 
 ## Installation
 ```
 pip install -r requirements.txt
 ```
+
+## Download data
+You can go to this [link](https://www.kaggle.com/datasets/sshikamaru/fruit-recognition) to download fruit dataset.
--- dataset.py
+++ dataset.py
@@ -3,41 +3,47 @@ from torch.utils.data import DataLoader
 import os
 from PIL import Image
 import tqdm
+from torchvision import transforms
+
 
 class FruitDataset(Dataset):
     def __init__(self, data_root, transform = None):
+        self.transform = transform
         self.img_paths = []
-        self.labels = []
+        self.targets = []
         self.class_to_index = {}
         for idx, class_name in enumerate(os.listdir(data_root)):
             class_dir = os.path.join(data_root, class_name)
             for img_name in os.listdir(class_dir):
                 img_path = os.path.join(class_dir, img_name)
                 self.img_paths.append(img_path)
-                self.labels.append(idx)
+                self.targets.append(idx)
             self.class_to_index[class_name] = idx
-        self.transform = transform
 
     def __len__(self):
         return len(self.img_paths)
     
     def __getitem__(self, idx):
         img_path = self.img_paths[idx]
-        label = self.labels[idx]
+        target = self.targets[idx]
         img = Image.open(img_path).convert('RGB')
         if self.transform:
             img = self.transform(img)
-        return img, label
+        return img, target
 
-def get_dataloader(data_root, batch_size, shuffle = True):
-    dataset = FruitDataset(data_root)
+def get_dataloader(dataset, batch_size, shuffle = True):
     dataloader = DataLoader(dataset, batch_size = batch_size, shuffle = shuffle)
     return dataloader
 
 if __name__ == '__main__':
     data_root = './data/train'
-    dataset = FruitDataset(data_root)
-    dataloader = get_dataloader(data_root, 16)
-    for samples, targets in tqdm(dataloader):
-        print(samples.shape)
-        pass
+    transform_train = transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+    ])
+    dataset = FruitDataset(data_root, transform=transform_train)
+    dataloader = get_dataloader(dataset, 16)
+    # for samples, targets in tqdm(dataloader, total = len(dataloader)):
+    #     pass
