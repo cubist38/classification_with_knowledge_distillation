212e821e4f0315dec46960265f265ad7d6b164e9
cubist38 <74866985+cubist38@users.noreply.github.com>
2023-05-22 21:56:46 +0700

    a

--- models/model.py
+++ models/model.py
@@ -1,4 +1,7 @@
-from models.efficientnet import *
+import torchvision
 
 def build_model(model_name: str, n_classes = 10):
-    return MyEfficientNet(model_name = model_name, num_classes = n_classes)
+    if model_name == 'efficientnet-b4':
+        return MyEfficientNet(model_name = model_name, num_classes = n_classes)
+    elif model_name == 'mobilenet_v2':
+        torchvision.models.mobilenet_v2(weights = MobileNet_V2_Weights.IMAGENET1K_V2)
