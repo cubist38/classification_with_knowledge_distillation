da0fad5e53376365ae072b83669107085d555420
cubist38 <74866985+cubist38@users.noreply.github.com>
2023-05-24 19:10:21 +0700

    a

--- models/efficientnet.py
+++ models/efficientnet.py
@@ -1,6 +1,10 @@
 import torch.nn as nn
 import torchvision
-from torchvision.models.efficientnet import EfficientNet_V2_L_Weights, EfficientNet_V2_S_Weights
+from torchvision.models.efficientnet import (EfficientNet_V2_L_Weights, 
+                                                EfficientNet_V2_S_Weights,
+                                                EfficientNet_B7_Weights,
+                                                EfficientNet_B4_Weights,)
+
 from models.base_model import BaseModel
 
 
@@ -17,7 +21,7 @@ class CustomEfficientNet(BaseModel):
                 nn.Linear(in_features=1280, out_features = num_classes, bias=True),
             )
             self.transform = EfficientNet_V2_L_Weights.IMAGENET1K_V1.transforms()
-        if model_name == 'efficientnet_v2_s':
+        elif model_name == 'efficientnet_v2_s':
             if pretrained:
                 self.model = torchvision.models.efficientnet_v2_s(weights = EfficientNet_V2_S_Weights.IMAGENET1K_V1)
             else:
@@ -27,3 +31,21 @@ class CustomEfficientNet(BaseModel):
                 nn.Linear(in_features=1280, out_features = num_classes, bias=True),
             )
             self.transform = EfficientNet_V2_S_Weights.IMAGENET1K_V1.transforms()
+        elif model_name == 'efficientnet_b7':
+            if pretrained:
+                self.model = torchvision.models.efficientnet_b7(weights = EfficientNet_B7_Weights.IMAGENET1K_V1)
+            else:
+                self.model = torchvision.models.efficientnet_b7(weights = None)
+            self.model.classifier = nn.Sequential(
+                nn.Dropout(p=0.5, inplace=True),
+                nn.Linear(in_features=2560, out_features = num_classes, bias=True),
+            )
+        elif model_name == 'efficientnet_b4':
+            if pretrained:
+                self.model = torchvision.models.efficientnet_b4(weights = EfficientNet_B4_Weights.IMAGENET1K_V1)
+            else:
+                self.model = torchvision.models.efficientnet_b4(weights = None)
+            self.model.classifier = nn.Sequential(
+                nn.Dropout(p=0.4, inplace=True),
+                nn.Linear(in_features=1792, out_features = num_classes, bias=True),
+            )
--- models/model.py
+++ models/model.py
@@ -3,9 +3,8 @@ from models.mobilenet import CustomMobileNet
 import torch.nn as nn
 
 def build_model(model_name: str, pretrained = True, n_classes = 10):
-    if model_name == 'efficientnet_v2_l':
+    model_type = model_name.split('_')[0]
+    if model_type == 'efficientnet':
         return CustomEfficientNet(pretrained = pretrained, num_classes = n_classes)
-    elif model_name == 'efficientnet_v2_s':
-        return CustomEfficientNet(pretrained = pretrained, num_classes = n_classes)
-    elif model_name == 'mobilenet_v2':
+    elif model_type == 'mobilenet':
         return CustomMobileNet(pretrained = pretrained, num_classes = n_classes)
