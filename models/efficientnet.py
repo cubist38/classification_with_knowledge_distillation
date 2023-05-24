1ffc8131a8e0f862af5910a2aaf2a901a1dc1050
cubist38 <74866985+cubist38@users.noreply.github.com>
2023-05-24 19:03:41 +0700

    a

--- models/efficientnet.py
+++ models/efficientnet.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
 import torchvision
-from torchvision.models.efficientnet import EfficientNet_V2_L_Weights
+from torchvision.models.efficientnet import EfficientNet_V2_L_Weights, EfficientNet_V2_S_Weights
 from models.base_model import BaseModel
 
 
@@ -17,3 +17,13 @@ class CustomEfficientNet(BaseModel):
                 nn.Linear(in_features=1280, out_features = num_classes, bias=True),
             )
             self.transform = EfficientNet_V2_L_Weights.IMAGENET1K_V1.transforms()
+        if model_name == 'efficientnet_v2_s':
+            if pretrained:
+                self.model = torchvision.models.efficientnet_v2_s(weights = EfficientNet_V2_S_Weights.IMAGENET1K_V1)
+            else:
+                self.model = torchvision.models.efficientnet_v2_s(weights = None)
+            self.model.classifier = nn.Sequential(
+                nn.Dropout(p=0.4, inplace=True),
+                nn.Linear(in_features=1280, out_features = num_classes, bias=True),
+            )
+            self.transform = EfficientNet_V2_S_Weights.IMAGENET1K_V1.transforms()
