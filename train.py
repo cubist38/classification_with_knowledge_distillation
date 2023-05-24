f0b482bed4bcc2602cfd085e2432692823492055
cubist38 <74866985+cubist38@users.noreply.github.com>
2023-05-24 19:33:40 +0700

    a

--- train.py
+++ train.py
@@ -39,7 +39,6 @@ def main(args):
     #transform = model.get_transform()
     transform = transforms.Compose([
         transforms.Resize(224, interpolation= PIL.Image.BICUBIC),
-        transforms.CenterCrop(256),
         transforms.ToTensor(),
         transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
     ])
