fa69c94d3fa5fda93d2df42eeda3320d4f68f748
cubist38 <74866985+cubist38@users.noreply.github.com>
2023-05-24 18:57:16 +0700

    a

--- models/model.py
+++ models/model.py
@@ -2,18 +2,6 @@ from models.efficientnet import CustomEfficientNet
 from models.mobilenet import CustomMobileNet
 import torch.nn as nn
 
-class base_model(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.model = None
-        self.transform = None
-
-    def forward(self, x):
-        return self.model(x)
-
-    def get_transform(self):
-        return self.transform
-
 def build_model(model_name: str, pretrained = True, n_classes = 10):
     if model_name == 'efficientnet_v2_l':
         return CustomEfficientNet(pretrained = pretrained, num_classes = n_classes)
