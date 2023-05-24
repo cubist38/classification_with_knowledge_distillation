1c3a25c54c4f4975994f38285871554e4fc033a0
cubist38 <74866985+cubist38@users.noreply.github.com>
2023-05-24 19:04:53 +0700

    a

--- models/model.py
+++ models/model.py
@@ -5,5 +5,7 @@ import torch.nn as nn
 def build_model(model_name: str, pretrained = True, n_classes = 10):
     if model_name == 'efficientnet_v2_l':
         return CustomEfficientNet(pretrained = pretrained, num_classes = n_classes)
+    elif model_name == 'efficientnet_v2_s':
+        return CustomEfficientNet(pretrained = pretrained, num_classes = n_classes)
     elif model_name == 'mobilenet_v2':
         return CustomMobileNet(pretrained = pretrained, num_classes = n_classes)
